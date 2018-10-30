cf7e00956cc29a2c7ed9bebe75d73e6c22626980
RadhaNR <43087448+RadhaNR@users.noreply.github.com>
2018-10-30 14:58:37 +0530

    Middleware

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+
 import './App.css';
 import ClassComponent from './app/components/ClassComponent';
 import ES5Func, { ES6Func } from './app/components/FunctionComponent';
@@ -6,32 +8,100 @@ import EventComponent from './app/components/EventComponent';
 import ReactLifeCycle from './app/components/ReactLifeCycle';
 import PureComponents from './app/components/PureComponent';
 import ShouldUpdate from './app/components/ShouldUpdate';
+import ParentComponent from './app/components/ParentComponent';
+import Param from './app/components/Param';
+import Ajax from './app/components/Ajax';
+import ProductList from './app/components/ProductList';
+import Cart from './app/components/Cart';
+import ControlledComponent from './app/components/ControlledComponent';
+import UnControlledComponent from './app/components/UnConrolledComponent';
+import UserPage from './app/components/UserPage';
+import UserComponent from './app/components/UserComponent';
+import ProductListComponent from './app/components/ProductListComponent';
+import FileNotFound from './app/components/FileNotFound';
+
+
+import UserList from './app/components/UserList';
+import User from './app/components/User';
+
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       name: 'HCL',
-      age: 42
+      age: 42,
+      showChild: false
     }
   }
 
   clickEvent = () => {
     this.setState({ name: 'HCL Tech', age: 100 });
-    this.setState({ age: 100 });
+    //this.setState({ age: 100 });
   }
+
+  showHide = () => {
+    this.setState({ showChild: !this.state.showChild });
+  }
+
   render() {
+    let divTest;
+    // if (this.state.showChild) {
+    //   divTest = <div>
+    //     <ClassComponent />
+    //     <ES5Func />
+    //     <ES6Func />
+    //     <EventComponent />
+    //     <ReactLifeCycle />
+    //     <PureComponents />
+    //     <ShouldUpdate />
+    //     <UserList />
+    //   </div>
+
+    // }
     return (
-      <div className="App">
-        <p>{this.state.name} ---- {this.state.age}</p>
-        <button onClick={this.clickEvent}>Change Parent on click</button>
-        <ClassComponent />
-        <ES5Func />
-        <ES6Func />
-        <EventComponent />
-        <ReactLifeCycle />
-        <PureComponents />
-        <ShouldUpdate />
+      <div className="container-fluid">
+      <button onClick={this.change}>Click here</button><br/>
+        <BrowserRouter>
+          <div className="row">
+            <div className="col-lg-3 col-md-3 col-sm-3">
+              <div><Link to="/pureComp"> PureComponent </Link></div>
+              <div><Link to="/pccp">p-c and c-p</Link></div>
+              <div><Link to="/userList"> User List </Link></div>
+              <div><Link to="/fromFun"> From Function </Link></div>
+              <div><Link to="/param/345345"> Param Component </Link></div>
+              <div><Link to="/ajax"> Ajax </Link></div>
+              <div><Link to="/productList" > Product List </Link></div>
+              <div><Link to="/cart"> Cart </Link></div>
+              <div><Link to="/controlled"> Controlled Component </Link></div>
+              <div><Link to="/unControlled"> UnControlled Component </Link></div>
+              <div><Link to="/hoc">HOC </Link></div>
+              <div><Link to="/userHoc">Load Users using HOC</Link></div>
+              <div><Link to="/productHoc">Load Products using HOC</Link></div>
+
+            </div>
+            <div className="col-lg-9 col-md-9 col-sm-9">
+              <Switch >
+                <Route path="/" component={PureComponents} exact />
+                <Route path="/pureComp" component={PureComponents} />
+                <Route path="/userList" component={UserList} />
+                <Route path="/fromFun" render={() => <div>Helllo</div>} />
+                <Route path="/param/:id" component={Param} />
+                <Route path="/ajax" component={Ajax} />
+                <Route path="/productList" component={ProductList} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/controlled" component={ControlledComponent} />
+                <Route path="/unControlled" component={UnControlledComponent} />
+                <Route path="/hoc" component={UserPage} />
+                <Route path="/userHoc" component={UserComponent} />
+                <Route path="/productHoc" component={ProductListComponent} />
+                <Route path="/pccp" component={ParentComponent} />
+
+                <Route component={FileNotFound} />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
